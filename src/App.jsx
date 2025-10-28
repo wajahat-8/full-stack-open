8d5c9d413749b20b744f45638a479bd52ed650e8
wajahat-8 <lowlevelcoding8@gmail.com>
2025-10-27 18:57:58 -0700

    Updated frontend

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import Footer from './components/Footer'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
+import loginServices from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username,setUsername]=useState('')
+  const [password,setPassword]=useState('')
+  const [user,setUser] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then(initialNotes => {
       setNotes(initialNotes)
     })
+  }, [])
+    useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
   }, [])
 
   const addNote = event => {
@@ -54,11 +66,71 @@ const App = () => {
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const handleLogin= async event=>{
+    event.preventDefault()
+    try {
+      const user = await loginServices.login({ username, password })
+       window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      setUser(user)
+      noteService.setToken(user.token)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+  }
+   const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  )
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+       <h2>Login</h2>
+      {!user && loginForm()}
+        {user && (
+      <div>
+        <p>{user.name} logged in</p>
+        {noteForm()}
+      </div>
+    )}
+      
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -73,10 +145,7 @@ const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+    
       <Footer />
     </div>
   )
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,14 +1,23 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes';
+let token = null
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then((response) => response.data)
 }
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then((response) => response.data)
+const create = async newObject=> {
+   const config = {
+    headers: { Authorization: token }
+  }
+
+ const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
 const update = (id, newObject) => {
@@ -20,4 +29,5 @@ export default {
   getAll,
   create,
   update,
+  setToken
 }
